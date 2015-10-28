4fe13046c7da6894ecf27f4cef7734239c49e23e
LenkaMilenka <lenakorccak@mail.ru>
2015-10-28 14:43:01 +0300

    edit last changes

--- validateForm.js
+++ validateForm.js
@@ -18,24 +18,24 @@ function showError(container, errorMessage) {
 
       resetError(elems.from.parentNode);
       if (!elems.from.value) {
-        showError(elems.from.parentNode, ' Show from who.');
+        showError(elems.from.parentNode, " Show from who.");
       }
 
       resetError(elems.password.parentNode);
       if (!elems.password.value) {
-        showError(elems.password.parentNode, ' Show password.');
+        showError(elems.password.parentNode, " Show password.");
       } else if (elems.password.value != elems.password2.value) {
-        showError(elems.password.parentNode, ' Passwords are not equal.');
+        showError(elems.password.parentNode, " Passwords are not equal.");
       }
 
       resetError(elems.to.parentNode);
       if (!elems.to.value) {
-        showError(elems.to.parentNode, ' Show, where.');
+        showError(elems.to.parentNode, " Show, where.");
       }
 
       resetError(elems.message.parentNode);
       if (!elems.message.value) {
-        showError(elems.message.parentNode, ' Text is absent.');
+        showError(elems.message.parentNode, " Text is absent.");
       }
 
     }
